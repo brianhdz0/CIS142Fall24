380bde310ca58696b33b725880774e3394421047
bhern <bhern@BRIANSPC>
2024-08-27 17:45:52 -0500

    Add project files.

---
+++ CIS142Fall24/CIS142Fall24.cpp
@@ -0,0 +1,10 @@
+// CIS142Fall24.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+   cout << "Brian Hernandez \n";
+}
+
